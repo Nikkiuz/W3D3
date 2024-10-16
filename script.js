316310c465d330b1666f37af03cdab6550e74832
Niccolò Albanese <nick_albanese@virgilio.it>
2024-10-16 16:24:15 +0200

    js not done

---
+++ script.js
@@ -0,0 +1,32 @@
+
+const form = document.getElementById('form')
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+    const newTask = document.getElementById('newTask').value
+   
+    const description = document.getElementById('description').value
+    // const cancelButton = 
+
+    const list = document.createElement('ul')
+    list.classList.add('list')
+    list.innerHTML = ` 
+                 <ul>
+                    <li>${newTask})</li>
+                    <p>${description}</p>
+                    <button>${'Cancel ✘'} </button>
+                    `
+
+    const listSection = document.getElementById('listSection')
+    listSection.appendChild(list)
+
+    // const button = document.querySelector('button')[1]
+    // button.addEventListener('click', function(){
+    //     display = none
+    // })
+
+    newTask.addEventListener('click', function () {
+        newTask.textContent.style.textDecoration = 'line-through'
+    })
+
+})
